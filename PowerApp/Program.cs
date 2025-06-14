0e728e937f76a46b1e68a19265971f30a6b62e1b
panosrodo <brandonroots@hotmail.gr>
2025-06-14 05:11:52 +0300

    primes app created

---
+++ PowerApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+
+namespace PowerApp
+{
+    /// <summary>
+    /// Υπολογίζει το b^power, π.χ. 2^10 = 1024
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myBase = 2;
+            int power = 10;
+            BigInteger result = 1;
+
+            for (int i = 1; i <= power; i++)
+            {
+                result *= myBase;   // result = result * myBase 
+            }
+
+            Console.WriteLine($"{myBase} ^ {power} = {result}");
+        }
+    }
+}
---
+++ PrimesApp/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PrimesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 20; i++)
+            {
+                Console.WriteLine($"{i}: is prime: {IsPrime(i)}");
+            }
+
+        }
+
+        public static bool IsPrime(int n)
+        {
+            bool prime = true;
+
+            for (int divider = 2; divider <= Math.Sqrt(n); divider++)
+            {
+                if ((n % divider) == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+
+            return prime;
+        }
+    }
+}
